d203e1153a7e13bd34e6a26b4908739f4c51a853
Anghel Laurentiu <57194590+laawrence01@users.noreply.github.com>
2022-02-01 18:34:41 +0200

    Added User Repo

---
+++ Proiect_DAW/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proiect_DAW.data;
+using Proiect_DAW.Models;
+using System.Linq;
+
+public class UserRepository
+{
+    protected readonly Proiect_DAWContext _context;
+    protected readonly DbSet<User> _table;
+
+    public UserRepository(Proiect_DAWContext context)
+    {
+        _context = context;
+        _table = context.Set<User>();
+    }
+
+
+    public User GetUserByPasswordAndUsername(string password, string username)
+    {
+        return _table.Where(x => x.Password == password  && x.Username == username).FirstOrDefault();
+    }
+
+    public User GetByUsername(string username)
+    {
+        return _table.Where(x => x.Username == username).FirstOrDefault();
+    }
+
+}
